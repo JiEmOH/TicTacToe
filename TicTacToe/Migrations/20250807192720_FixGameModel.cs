fd71c88f29df342c76b59660589d3e34ee0c0f37
Alexander <alexander.eliseev99@mail.ru>
2025-08-08 00:08:58 +0300

    add board state API with static 3x3 matrix

---
+++ TicTacToe/Migrations/20250807192720_FixGameModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TicTacToe.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixGameModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Games",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "WinnerId",
+                table: "Games",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_Player1Id",
+                table: "Games",
+                column: "Player1Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_Player2Id",
+                table: "Games",
+                column: "Player2Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_WinnerId",
+                table: "Games",
+                column: "WinnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Players_Player1Id",
+                table: "Games",
+                column: "Player1Id",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Players_Player2Id",
+                table: "Games",
+                column: "Player2Id",
+                principalTable: "Players",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Players_WinnerId",
+                table: "Games",
+                column: "WinnerId",
+                principalTable: "Players",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Players_Player1Id",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Players_Player2Id",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Players_WinnerId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_Player1Id",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_Player2Id",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_WinnerId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "WinnerId",
+                table: "Games");
+        }
+    }
+}
